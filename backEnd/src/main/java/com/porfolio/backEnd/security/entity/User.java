c5cb0d5862acafba91df5cbda32bb4215901e075
sistekor17 <jamesserrato17@gmail.com>
2023-04-24 18:10:47 -0300

    Desarrollo de clase MainUser 75%

---
+++ backEnd/src/main/java/com/porfolio/backEnd/security/entity/User.java
@@ -0,0 +1,43 @@
+package com.porfolio.backEnd.security.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author JAMES
+ */
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    private String nickname;
+    @Column(unique = true)
+    private String name;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+//    Cardinalidad de tablas: ROl - Ususario relacion de muchos a muchos.
+    @ManyToMany(fetch = FetchType.EAGER)
+//  Este es el codigo de la tabla intermedia que va a tener el nombre  "user_rol", la cual contiene dos columnas
+    @JoinTable(name = "user_rol", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Rol> roles = new HashSet<>();
+}
---
+++ backEnd/src/main/java/com/porfolio/backEnd/security/enums/RolName.java
@@ -0,0 +1,10 @@
+
+package com.porfolio.backEnd.security.enums;
+
+/**
+ *
+ * @author JAMES
+ */
+public enum RolName {
+    ROLE_ADMIN, ROLE_USER
+}
